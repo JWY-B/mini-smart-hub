546e0386fe248113b799338a3459430546902f69
jwy <2660243285@qq.com>
2025-08-17 19:14:27 +0800

    LCD代码

--- User/main.c
+++ User/main.c
@@ -1,21 +1,27 @@
-#include "stm32f10x.h"                  // Device header
+/*
+ * @Author: jwy 2660243285@qq.com
+ * @Date: 2025-08-16 18:13:19
+ * @LastEditTime: 2025-08-17 18:53:11
+ * @FilePath: \mini-smart-hub\User\main.c
+ * @Description:
+ */
+#include "stm32f10x.h" // Device header
 
 int main(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	//开启GPIOA的时钟
-															//使用各个外设前必须开启时钟，否则对外设的操作无效
-	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); // 开启GPIOA的时钟
+														  // 使用各个外设前必须开启时钟，否则对外设的操作无效
+
 	/*GPIO初始化*/
-	GPIO_InitTypeDef GPIO_InitStructure;					//定义结构体变量
-	
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;		//GPIO模式，赋值为推挽输出模式
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;				//GPIO引脚，赋值为第0号引脚
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		//GPIO速度，赋值为50MHz
-	
-	GPIO_Init(GPIOA, &GPIO_InitStructure);					//将赋值后的构体变量传递给GPIO_Init函数
-	GPIO_ResetBits(GPIOA,GPIO_Pin_1);
-	while(1)
+	GPIO_InitTypeDef GPIO_InitStructure; // 定义结构体变量
+
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;  // GPIO模式，赋值为推挽输出模式
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;		  // GPIO引脚，赋值为第0号引脚
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // GPIO速度，赋值为50MHz
+
+	GPIO_Init(GPIOA, &GPIO_InitStructure); // 将赋值后的构体变量传递给GPIO_Init函数
+	GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+	while (1)
 	{
-		
 	}
 }
