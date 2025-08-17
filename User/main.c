7175b601e9bfa8bd2ef653cc4dab534e3b29da82
jwy <2660243285@qq.com>
2025-08-17 18:44:46 +0800

    STM32项目创建

---
+++ User/main.c
@@ -0,0 +1,21 @@
+#include "stm32f10x.h"                  // Device header
+
+int main(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	//开启GPIOA的时钟
+															//使用各个外设前必须开启时钟，否则对外设的操作无效
+	
+	/*GPIO初始化*/
+	GPIO_InitTypeDef GPIO_InitStructure;					//定义结构体变量
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;		//GPIO模式，赋值为推挽输出模式
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;				//GPIO引脚，赋值为第0号引脚
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		//GPIO速度，赋值为50MHz
+	
+	GPIO_Init(GPIOA, &GPIO_InitStructure);					//将赋值后的构体变量传递给GPIO_Init函数
+	GPIO_ResetBits(GPIOA,GPIO_Pin_1);
+	while(1)
+	{
+		
+	}
+}
