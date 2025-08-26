0a41b1d48cf2dcb373bd49e7325c6368bf696646
jwy <2660243285@qq.com>
2025-08-26 19:51:12 +0800

    移植FreeRTOS

--- User/stm32f10x_it.c
+++ User/stm32f10x_it.c
@@ -23,6 +23,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x_it.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
 /** @addtogroup STM32F10x_StdPeriph_Template
   * @{
@@ -105,9 +107,9 @@ void UsageFault_Handler(void)
   * @param  None
   * @retval None
   */
-void SVC_Handler(void)
-{
-}
+//void SVC_Handler(void)
+//{
+//}
 
 /**
   * @brief  This function handles Debug Monitor exception.
@@ -123,9 +125,9 @@ void DebugMon_Handler(void)
   * @param  None
   * @retval None
   */
-void PendSV_Handler(void)
-{
-}
+//void PendSV_Handler(void)
+//{
+//}
 
 /**
   * @brief  This function handles SysTick Handler.
@@ -134,6 +136,11 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
+	if (xTaskGetSchedulerState() != taskSCHEDULER_NOT_STARTED) 
+    {
+        xPortSysTickHandler();
+    }
+
 }
 
 /******************************************************************************/
